508820c59323b2125a4d106ddb9fd8d7b18c10fa
Javiergonb <javiergonb3@gmail.com>
2025-01-06 23:36:08 -0300

    feat: Add basic number display support

--- logic.js
+++ logic.js
@@ -1,7 +1,33 @@
 //ADD SUBSTRACT MULT AND DIVISION!
 //TODO: DECIMAL OPERAND SUPPORT
 
-let operate = function(leftOperand,righOperand,operator){
+const inputDisplay = document.querySelector("#number-input");
+const numberButtons = document.querySelectorAll("#number-button");
+const opsButtons = document.querySelectorAll("#ops-button");
+const lastOP = document.querySelector("#display")
+const equalButton = document.querySelector("#equal-button")
+
+let displayedNumber = "0";
+let leftOperand;
+let righOperand;
+let operator;
+inputDisplay.value = displayedNumber
+
+
+//Display functions
+const updateDisplay = function(number){
+    if(displayedNumber === "0"){
+        displayedNumber = number;
+    }
+    else{
+        displayedNumber += number;
+    }
+    inputDisplay.value = displayedNumber;
+}
+
+
+//Logic Functions
+const operate = function(leftOperand,righOperand,operator){
     switch(operator){
         case '+' :
             return leftOperand + righOperand;
@@ -16,3 +42,35 @@ let operate = function(leftOperand,righOperand,operator){
             return leftOperand / righOperand;
     }
 };
+//Setup Functions
+
+const setUpNumberInput = function (){
+    numberButtons.forEach((button) =>{
+        button.addEventListener("click",(e) => {
+            let number = button.textContent;
+            updateDisplay(number);
+            lastPressed = e.target.textContent;
+        });
+    })
+    
+}
+
+
+const setUpOpsInput = function(){
+    opsButtons.forEach((button) =>{
+        button.addEventListener("click",(e) =>{
+            if(lastPressed)
+            leftOperand = displayedNumber;
+            operator = e.target.textContent;
+            displayedNumber = ""
+            
+        })
+    })
+}
+
+const setUpEqualInput = function() {
+    equalButton.addEventListener("click", handleEqual())
+}
+
+setUpNumberInput();
+setUpOpsInput();
