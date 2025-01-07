aacd10e147dbb38b0afdc655ca28a17326b2caf4
Javiergonb <javiergonb3@gmail.com>
2025-01-07 10:38:05 -0300

    feat: Add basic sum,res,div and mult functions with the front end
    Created the events for clicking numbers and operating them as well as the clear button

--- logic.js
+++ logic.js
@@ -4,14 +4,17 @@
 const inputDisplay = document.querySelector("#number-input");
 const numberButtons = document.querySelectorAll("#number-button");
 const opsButtons = document.querySelectorAll("#ops-button");
-const lastOP = document.querySelector("#display")
-const equalButton = document.querySelector("#equal-button")
+const lastOP = document.querySelector("#display");
+const equalButton = document.querySelector("#equal-button");
+const clearButton = document.querySelector("#clear-button");
+const delButton = document.querySelector("#del-button");
 
 let displayedNumber = "0";
 let leftOperand;
 let righOperand;
 let operator;
-inputDisplay.value = displayedNumber
+let operation = "";
+inputDisplay.value = displayedNumber;
 
 
 //Display functions
@@ -25,23 +28,42 @@ const updateDisplay = function(number){
     inputDisplay.value = displayedNumber;
 }
 
+const updateLastOp = function(){
+    lastOP.textContent = operation;
+}
 
 //Logic Functions
-const operate = function(leftOperand,righOperand,operator){
+const operate = function(left,right){
     switch(operator){
         case '+' :
-            return leftOperand + righOperand;
+            return left + right;
         
         case '-':
-            return leftOperand - righOperand;
+            return left - right;
 
         case '*':
-            return leftOperand * righOperand;
+            return left * right;
 
         case '/':
-            return leftOperand / righOperand;
+            return left / right;
     }
 };
+
+//Handler functions
+
+const handleEqual = function(){
+    righOperand = displayedNumber;
+    operation += " " + righOperand + "=";
+    let left = parseInt(leftOperand);
+    let right = parseInt(righOperand);
+    const result = operate(left,right);
+    console.log(typeof result);
+    displayedNumber = "0";
+    updateDisplay(result);
+    updateLastOp();
+}
+
+
 //Setup Functions
 
 const setUpNumberInput = function (){
@@ -49,7 +71,6 @@ const setUpNumberInput = function (){
         button.addEventListener("click",(e) => {
             let number = button.textContent;
             updateDisplay(number);
-            lastPressed = e.target.textContent;
         });
     })
     
@@ -59,18 +80,29 @@ const setUpNumberInput = function (){
 const setUpOpsInput = function(){
     opsButtons.forEach((button) =>{
         button.addEventListener("click",(e) =>{
-            if(lastPressed)
             leftOperand = displayedNumber;
             operator = e.target.textContent;
+            operation = leftOperand + " " + operator;
+            updateLastOp();
             displayedNumber = ""
-            
         })
     })
 }
 
 const setUpEqualInput = function() {
-    equalButton.addEventListener("click", handleEqual())
+    equalButton.addEventListener("click", handleEqual)
 }
 
+const setUpClearInput = function(){
+    clearButton.addEventListener("click",() => {
+        operation = null;
+        updateLastOp();
+        displayedNumber = ""
+        updateDisplay("0")
+    })
+}
+
+setUpClearInput();
 setUpNumberInput();
 setUpOpsInput();
+setUpEqualInput();
